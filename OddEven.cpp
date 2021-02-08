622d97bd4a5f51660f0c44d1b9b1e940dc2d1396
Dahlia Ghonim <50227388+DahliaGhonim@users.noreply.github.com>
2021-02-08 08:20:00 +0200

    Create OddEven.cpp

---
+++ OddEven.cpp
@@ -0,0 +1,25 @@
+// Problem C
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, num, min = INT_MAX;
+	long long sum = 0;
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> num;
+		sum += num;
+
+		if (num % 2 != 0 && num < min)
+			min = num;
+	}		
+
+	// if sum is odd, subtract the smallest odd number
+	if (sum % 2 != 0)
+		sum -= min;
+
+	cout << sum << endl;
+}
