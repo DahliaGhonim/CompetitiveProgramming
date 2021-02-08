f28d576440470f32d2683fefa234a1be0b92b147
Dahlia Ghonim <50227388+DahliaGhonim@users.noreply.github.com>
2021-02-08 08:55:07 +0200

    Problem N

---
+++ Countryside.cpp
@@ -0,0 +1,27 @@
+// Problem N
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, a, b;
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a;
+		if (i == n - 2)
+			b = a;
+	}
+
+	if (a == 0)
+		cout << "UP" << endl;
+	else if (a == 15)
+		cout << "DOWN" << endl;
+	else if (n == 1)
+		cout << -1 << endl;
+	else if (a < b)
+		cout << "DOWN" << endl;	
+	else if (a > b)
+		cout << "UP" << endl;
+}
