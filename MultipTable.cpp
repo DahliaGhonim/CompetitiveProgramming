21d1216399d0d1ea092609ae9cd99852c10882e3
Dahlia Ghonim <50227388+DahliaGhonim@users.noreply.github.com>
2021-02-08 08:33:01 +0200

    Create MultipTable.cpp

---
+++ MultipTable.cpp
@@ -0,0 +1,15 @@
+// Problem E
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, x, count = 0;
+	cin >> n >> x;
+
+	for (int i = 1; i <= n && i <= x; i++)
+		if (x % i == 0 && x / i <= n)
+			count++;
+
+	cout << count << endl;
+}
