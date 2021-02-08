3ea0687810c089355a260c31a7f6f91764615e8c
Dahlia Ghonim <50227388+DahliaGhonim@users.noreply.github.com>
2021-02-08 09:27:25 +0200

    Problem R

---
+++ Taymyr.cpp
@@ -0,0 +1,17 @@
+// Problem R
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, m, z;
+	cin >> n >> m >> z;
+
+	int count = 0;
+
+	for (int i = 1; i <= z; i++)
+		if (i % n == 0 && i % m == 0)
+			count++;
+
+	cout << count << endl;
+}
