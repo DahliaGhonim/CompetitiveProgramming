3b06e9988bd90e7cf1bddf2cfa48201e14c0cdf1
Dahlia Ghonim <50227388+DahliaGhonim@users.noreply.github.com>
2021-02-08 08:35:13 +0200

    Create InfiniteSequence.cpp

---
+++ InfiniteSequence.cpp
@@ -0,0 +1,18 @@
+// Problem F
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	cin >> a >> b >> c;
+
+	if ((c == 0 && a != b) || (c > 0 && b < a) || (c < 0 && b > a))
+		cout << "NO" << endl;
+	else if (b == a)
+		cout << "YES" << endl;
+	else if ((b - a) % c == 0)
+		cout << "YES" << endl;
+	else
+		cout << "NO" << endl;
+}
