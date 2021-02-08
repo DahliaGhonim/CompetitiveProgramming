3d5277e8d7acca0c28beccdb7510cde4d95f5aaf
Dahlia Ghonim <50227388+DahliaGhonim@users.noreply.github.com>
2021-02-09 01:50:12 +0200

    Problem L

--- mainL.cpp
+++ mainL.cpp
@@ -1,46 +1,23 @@
 // Problem L
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main()
 {
-	int n, m;
+	int n, m, a, b, d = 0;
 	cin >> n >> m;
 
-	int* arr = new int [n * 2];
-	for (int i = 0; i < n * 2; i++)
-		cin >> arr[i];
-
-	int a = 0, b = 0, indx, max;
-	bool flag;
-
-	for (int i = 0; i < n * 2; i += 2)
+	for (int i = 0; i < n; i++)
 	{
-		max = b;
-		flag = false;
+		cin >> a >> b;
 
-		for (int j = i; j < n * 2; j += 2)
-			if (arr[j] >= a && arr[j] <= b && arr[j + 1] > max)
-			{
-				indx = j;
-				max = arr[j + 1];
-				flag = true;
-			}
-
-		if (flag)
-		{
-			a = arr[indx];
-			b = arr[indx + 1];
-			i = indx;
-		}
-		else
-			break;
+		if (a <= d)
+			d = fmax(d, b);
 	}
 
-	if (b == m)
+	if (d == m)
 		cout << "YES" << endl;
 	else
 		cout << "NO" << endl;
-
-	delete[] arr;
 }
