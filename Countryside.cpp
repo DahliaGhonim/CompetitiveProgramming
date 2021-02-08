0535c5790d75f160acd75794b75daa25ac3ead5a
Dahlia Ghonim <50227388+DahliaGhonim@users.noreply.github.com>
2021-02-08 09:16:31 +0200

    Problem N

--- Countryside.cpp
+++ Countryside.cpp
@@ -7,13 +7,10 @@ int main()
 	int n, a, b;
 	cin >> n;
 
-	for (int i = 0; i < n; i++)
-	{
-		cin >> a;
-		if (i == n - 2)
-			b = a;
-	}
-
+	for (int i = 1; i < n; i++)
+		cin >> b;	// the number before last one
+	cin >> a;		// the last number
+	
 	if (a == 0)
 		cout << "UP" << endl;
 	else if (a == 15)
